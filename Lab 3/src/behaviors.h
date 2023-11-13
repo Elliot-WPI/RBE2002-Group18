5bb6d41019a292cfefaacf9ea26d82fd7ab66cfa
ncaughron17 <143098908+ncaughron17@users.noreply.github.com>
2023-11-13 18:01:28 -0500

    Update behaviors.h

--- Lab 3/src/behaviors.h
+++ Lab 3/src/behaviors.h
@@ -5,9 +5,9 @@
 
 class Behaviors{
     private:
-        int threshold = 500;
+        int threshold = 400;
         int zThreshold = 1200;
-        int threshold_pick_up = 1500;
+        int threshold_pick_up = 1500;//redundant (ignore)
         int data[3] = {0};
         enum ROBOT_STATE {IDLE, DRIVE, REVERSE, TURN};
         ROBOT_STATE robot_state = IDLE; //initial state: IDLE
