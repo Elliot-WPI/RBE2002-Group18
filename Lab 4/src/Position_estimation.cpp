fbe0a60d7d6a59f5dc781f0bf0e1f7f90ef42ad8
Elliot-WPI <eghidali@gmail.com>
2023-11-15 16:07:40 -0500

    commited

--- Lab 4/src/Position_estimation.cpp
+++ Lab 4/src/Position_estimation.cpp
@@ -8,6 +8,7 @@ float theta = 0;
 unsigned long time_prev = millis();
 unsigned long time_now = 0;
 
+
 void Position::Init(void)
 {
     time_prev = millis();
@@ -40,9 +41,19 @@ void Position::PrintPose(void)
 
 void Position::UpdatePose(float target_speed_left, float target_speed_right)
 {
+    double w;
+    double R;
+    double V;
     time_now = millis();
     if(time_now - time_prev >= 50) //update every 50ms for practical reasons
-    {
+    {    
+        R = (target_speed_right+target_speed_left)/2*(target_speed_right-target_speed_left);
+        w = (target_speed_left-target_speed_right)/l;
+        theta = theta + w * 0.05;
+        V = (target_speed_left+target_speed_right)/2;
+        x = x + V*cos(theta)*0.05;
+        y = y + V*sin(theta)*0.05;
+
         //assignment
     }
 }
