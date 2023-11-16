d0a7e7b6485aa904380ff2024547a9396cad4e4a
ncaughron17 <143098908+ncaughron17@users.noreply.github.com>
2023-11-15 21:28:38 -0500

    Fixing the turning

--- Lab 4/src/Speed_controller.h
+++ Lab 4/src/Speed_controller.h
@@ -9,7 +9,7 @@ class SpeedController{
         const float Ki = 0.1; 
         float E_left = 0; 
         float E_right = 0;
-        int counts = 0; //assignment
+        int counts = ((141 * 3.14/2)/(70 * 3.14)) * 1440; //assignment
 
     public:
         void Init(void);
